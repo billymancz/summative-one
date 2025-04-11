79181b7218d74b1215a174cc6cc1e5823acd02da
billy mancz <billy@mancz.com>
2025-04-11 10:03:48 +0100

    adding stuff for github actions tests etc

---
+++ test_fizzbuzz.py
@@ -0,0 +1,14 @@
+import pytest
+from fizzbuzz import fizzbuzz 
+
+def test_fizzbuzz_returns_number():
+    assert fizzbuzz(1) == "1"
+
+def test_fizzbuzz_returns_fizz():
+    assert fizzbuzz(3) == "Fizz"
+
+def test_fizzbuzz_returns_buzz():
+    assert fizzbuzz(5) == "Buzz"
+
+def test_fizzbuzz_returns_fizzbuzz():
+    assert fizzbuzz(15) == "FizzBuzz"
